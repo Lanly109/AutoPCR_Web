41f30b4595b792e1293b659d7e6d00181b6c2b50
Lanly109 <1094916227@qq.com>
2025-05-11 22:16:04 +0800

    fix: toast position top right

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import theme from './theme'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     // <React.StrictMode>
-    <ChakraProvider toastOptions={{ defaultOptions: { position: 'bottom', duration: 2500 } }} theme={theme}>
+    <ChakraProvider toastOptions={{ defaultOptions: { position: 'top-right', duration: 2500, isClosable: true } }} theme={theme}>
         <NiceModal.Provider>
             <App />
         </NiceModal.Provider>
